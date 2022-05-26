242c1ea449b8a1237f2aaff6e6becf5ab729e1c9
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-05-26 18:56:25 +0400

    Add files via upload

---
+++ Encapsulation/Set.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Student
+    {
+        private int[] _grades;
+        public int[] Grades
+        {
+            get
+            {
+                return _grades;
+            }
+            set
+            {
+
+
+            }
+            static void [] GradesPrint(int[] num)
+        {
+
+
+            foreach (var item in Grades)
+            {
+                if (item % 2 == 0)
+                {
+
+                    Console.WriteLine(_grades);
+                }
+            }
+        }return 
+
+            }
+        }
+
+    }
+
---
+++ Encapsulation/Set2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student person1 = new Student();
+            person1.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(person1.Grades);
+        }
+    }
+}
